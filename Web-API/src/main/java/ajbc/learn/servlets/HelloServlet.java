b90f7c82989d5515879265afc0344972d0bc263a
Liran Hadad <liranh6@gmail.com>
2022-06-29 09:35:07 +0300

    adding tomcat server and check simple api hello

---
+++ Web-API/src/main/java/ajbc/learn/servlets/HelloServlet.java
@@ -0,0 +1,26 @@
+package ajbc.learn.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("Hello from Liran!");
+		
+	}
+
+	
+}
